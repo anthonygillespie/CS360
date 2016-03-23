f3f66ae013d8a836899148b5ea09fb828220f0f8
Anthony Gillespie <neoanthum@gmail.com>
2016-03-23 14:14:38 +0000

    17

--- new_lab9/angular/routes/index.js
+++ new_lab9/angular/routes/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Comment = mongoose.model('Comment');
+
+router.post('/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+    res.json(comment);
+  });
+});
+
+router.get('/comments', function(req, res, next) {
+  Comment.find(function(err, comments){
+    if(err){ return next(err); }
+    res.json(comments);
+  });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
